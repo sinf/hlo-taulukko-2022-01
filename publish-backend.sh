f67240c9f6d923f2441f7838885d87bc823a523b
ArhoM <tyonhaku@amahl.fi>
2022-01-27 03:35:17 +0200

    heroku publish scripts

---
+++ publish-backend.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -x -e
+source heroku-apps.cfg
+cd backend
+app=$heroku_backend_app
+heroku container:push -a $app web
+heroku container:release -a $app web
---
+++ publish-frontend.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -x -e
+source heroku-apps.cfg
+cd hlo-taulukko-app
+app2=$heroku_backend_app
+app=$heroku_frontend_app
+heroku container:push --arg REACT_APP_BACKEND_URL="https://$app2.herokuapp.com"  -a $app web
+heroku container:release -a $app web
